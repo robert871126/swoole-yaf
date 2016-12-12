968afb6ed6b0f8389f2f1237ebd0302c3eb68285
robert <robert871126@163.com>
2016-12-12 17:34:34 +0800

    删除application下无用文件

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -4,7 +4,6 @@ class IndexController extends Yaf\Controller_Abstract
 {
     public function indexAction()
     {
-        echo 'success' . PHP_EOL;
-        var_dump(M('User')->find());
+        echo 'Hello World!';
     }
 }
